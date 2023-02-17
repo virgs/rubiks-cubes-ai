18a340faaaee130b1a1c550fefd0fb849d9f92f2
virgs <gmoraes@outlook.com>
2023-02-17 12:18:30 -0800

    All rotations are working

--- src/engine/face-rotation.ts
+++ src/engine/face-rotation.ts
@@ -3,5 +3,6 @@ import type { Sides } from "./sides";
 export type FaceRotation = {
     side: Sides;
     layer?: number;
+    duration?: number;
     clockwiseDirection?: boolean;
 };
--- src/renderers/cube-renderer.ts
+++ src/renderers/cube-renderer.ts
@@ -1,12 +1,14 @@
 import type { PocketCube } from "@/engine/pocket-cube";
-import { BoxGeometry, EdgesGeometry, Group, LineBasicMaterial, LineSegments, MathUtils, Mesh, MeshStandardMaterial, Scene, Vector3 } from "three";
+import { BoxGeometry, EdgesGeometry, Group, LineBasicMaterial,
+     LineSegments, MathUtils, Mesh, MeshStandardMaterial, Scene, Vector3 } from "three";
 //https://github.com/tweenjs/tween.js/blob/main/docs/user_guide.md
 import * as Tween from '@tweenjs/tween.js'
+import type { FaceRotation } from "@/engine/face-rotation";
+import { Sides } from "@/engine/sides";
 
 type RotationTween = {
     rotation: number
 }
-
 
 //https://github.com/SuboptimalEng/gamedex/blob/19b0466ad30ef0fb6c760eb78f74e6cf64fa1a73/08-rubiks-cube/src/lib/Cube.js
 export class CubeRenderer {
@@ -64,155 +66,61 @@ export class CubeRenderer {
                         this.rubiksCubeGroup.add(cubeletGroup)
                     })));
         this.scene.add(this.rubiksCubeGroup);
-        this.rubiksCubeGroup.rotation.x = Math.PI / 1;
-        this.rubiksCubeGroup.rotation.y = -Math.PI / 4;                    
-        this.rotate();
     }
 
     public getCube(): Group {
         return this.rubiksCubeGroup;
     }
 
-    public rotate(): void {
-        const rotationGroup = new Group();
-        Array.from(this.rubiksCubeGroup.children)
-            .map(child => child as Mesh)
-            .sort((first: Mesh, second: Mesh) => first.position.y - second.position.y)
-            .filter((_, index) => index < this.dimension * this.dimension)
-            .forEach(cubelet => rotationGroup.add(cubelet));
-        console.log(rotationGroup.children.length, this.rubiksCubeGroup.children.length);
-        this.scene.add(rotationGroup);
-        rotationGroup.setRotationFromEuler(this.rubiksCubeGroup.rotation.clone());
+    public async rotate(faceRotation: FaceRotation): Promise<void> {
+        let sortFunction = (a, b) => b - a;
+        let targetAngle = (Math.PI / 2) * (faceRotation.clockwiseDirection? -1 : 1 )
 
-        const start: RotationTween = { rotation: 0 };
-        const prev: RotationTween = { rotation: 0 };
-        const end: RotationTween = { rotation: Math.PI / 2 };
-        const axis = new Vector3(0,1,0).normalize()
+        let axisName = 'y'
+        if (faceRotation.side === Sides.BACK || faceRotation.side === Sides.DOWN || faceRotation.side === Sides.LEFT) {
+            sortFunction = (a, b) => a - b;
+            targetAngle *= -1;
+        }
+        if (faceRotation.side === Sides.BACK || faceRotation.side === Sides.FRONT) {
+            axisName = 'z'
+        } else if (faceRotation.side === Sides.RIGHT || faceRotation.side === Sides.LEFT) {
+            axisName = 'x'
+        }
 
-        // this.rubiksCubeGroup.parent.add(cubeGroup)
+        const axis = new Vector3(0,0,0);
+        axis[axisName] = 1;
+
+        return new Promise((resolve) => {
+            const rotationGroup = new Group();
+            Array.from(this.rubiksCubeGroup.children)
+                .map(child => child as Mesh)
+                .sort((first: Mesh, second: Mesh) => sortFunction(first.position[axisName], second.position[axisName]))
+                .filter((_, index) => index < this.dimension * this.dimension)
+                .forEach(cubelet => rotationGroup.add(cubelet));
+            console.log(rotationGroup.children.length, this.rubiksCubeGroup.children.length);
+            this.scene.add(rotationGroup);
+            rotationGroup.setRotationFromEuler(this.rubiksCubeGroup.rotation.clone());
     
-        const duration = 3000;
-        this.animation = new Tween.Tween(start)
-          .to(end, duration)
-          .easing(Tween.Easing.Quadratic.InOut)
-          .onUpdate((item: RotationTween) => {
-            rotationGroup.position.applyAxisAngle(axis, item.rotation - prev.rotation);
-            rotationGroup.rotateOnWorldAxis(axis, item.rotation - prev.rotation);   
-            // NOTE: Keep track of the previous rotation for tweening.
-            prev.rotation = item.rotation;
-            rotationGroup.updateMatrixWorld();
-          })
-          .onComplete(() => {
-            console.log('isOver');
-            this.rubiksCubeGroup.add(...rotationGroup.children)
-            this.scene.remove(rotationGroup);
-            this.animation = undefined;
-            this.rotate();
-          })
-          .start();
-
-
-        // const pivot = new Object3D();
-        // pivot.position.set(0, 1, 0);
-        // pivot.rotation.set(0, 0, 0);
-
-        // const angle = delta * CubeRenderer.radiansPerSecond;
-        // Array.from(this.cube.children)
-        //     .map(child => child as Mesh)
-        //     .sort((first: Mesh, second: Mesh) => second.position.y - first.position.y)
-        //     .filter((_, index) => index < 1)//this.dimension * this.dimension)
-        //     .forEach(mesh => {
-        //         // pivot.attach(mesh)
-        //         // // mesh.getWorldPosition
-
-        //         // mesh.rotateY(angle)
-
-        //         // mesh.position.x = Math.sin(mesh.rotation.y) * 0.5
-        //         // mesh.position.z = Math.cos(mesh.rotation.y) * 0.5
-
-
-        //         mesh.rotateOnWorldAxis(new Vector3(0, 1, 0).normalize(), angle);
-
-        //     });
-
-        // pivot.rotateY(angle)
-
-        // this.cube.position.x = Math.cos(this.cube.rotation.y) * 0.5
-        // this.cube.position.z = Math.sin(this.cube.rotation.y) * 0.5
-
-        // this.cube.rotateY(delta * CubeRenderer.radiansPerSecond);
+            const start: RotationTween = { rotation: 0 };
+            const prev: RotationTween = { rotation: 0 };
+            const end: RotationTween = { rotation: targetAngle};
+    
+            this.animation = new Tween.Tween(start)
+              .to(end, faceRotation.duration)
+              .easing(Tween.Easing.Quadratic.InOut)
+              .onUpdate((item: RotationTween) => {
+                rotationGroup.position.applyAxisAngle(axis, item.rotation - prev.rotation);
+                rotationGroup.rotateOnWorldAxis(axis, item.rotation - prev.rotation);   
+                prev.rotation = item.rotation;
+                rotationGroup.updateMatrixWorld();
+              })
+              .onComplete(() => {
+                this.rubiksCubeGroup.add(...rotationGroup.children)
+                this.scene.remove(rotationGroup);
+                this.animation = undefined;
+                resolve();
+              })
+              .start();
+        });
     }
-
 }
-
-//
-  //Select the plane of cubes that aligns with clickVector
-//   // on the given axis
-//   function setActiveGroup(axis) {
-//     if(clickVector) {
-//       activeGroup = [];
-
-//       allCubes.forEach(function(cube) {
-//         if(nearlyEqual(cube.rubikPosition[axis], clickVector[axis])) { 
-//           activeGroup.push(cube);
-//         }
-//       });
-//     } else {
-//       console.log("Nothing to move!");
-//     }
-//   }
-
-
-
-// //
-// var startNextMove = function() {
-//     var nextMove = moveQueue.pop();
-
-//     if(nextMove) {
-//       clickVector = nextMove.vector;
-      
-//       var direction = nextMove.direction || 1,
-//           axis = nextMove.axis;
-
-//       if(clickVector) {
-
-//         if(!isMoving) {
-//           isMoving = true;
-//           moveAxis = axis;
-//           moveDirection = direction;
-
-//           setActiveGroup(axis);
-
-//           pivot.rotation.set(0,0,0);
-//           pivot.updateMatrixWorld();
-//           scene.add(pivot);
-
-//           activeGroup.forEach(function(e) {
-//             THREE.SceneUtils.attach(e, scene, pivot);
-//           });
-
-//           currentMove = nextMove;
-//         } else {
-//           console.log("Already moving!");
-//         }
-//       } else {
-//         console.log("Nothing to move!");
-//       }
-//     } else {
-//       moveEvents.trigger('deplete');
-//     }
-//   }
-
-//   function doMove() {
-//     //Move a quarter turn then stop
-//     if(pivot.rotation[moveAxis] >= Math.PI / 2) {
-//       //Compensate for overshoot. TODO: use a tweening library
-//       pivot.rotation[moveAxis] = Math.PI / 2;
-//       moveComplete();
-//     } else if(pivot.rotation[moveAxis] <= Math.PI / -2) {
-//       pivot.rotation[moveAxis] = Math.PI / -2;
-//       moveComplete()
-//     } else {
-//       pivot.rotation[moveAxis] += (moveDirection * rotationSpeed);
-//     }
-//   }
