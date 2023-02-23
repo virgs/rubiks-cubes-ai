594bce1cb3c30475114cf0a19bb8ebe5e1f4bc66
guilherme moraes <guilherme.moraes@outlook.com>
2023-02-23 14:00:26 -0800

    Try to fix build fonts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "/rubiks-cubes-ai",
-  assetsInclude: ["**/*.m4a", "**/*/json"],
+  assetsInclude: ["**/*.m4a", "**/*.json"],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
